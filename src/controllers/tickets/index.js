459f54f4f4f4523c024c0d81233d94cd423c8612
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-28 18:41:59 -0300

    feat: :sparkles: add filters

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,9 +1,11 @@
 export function index({ request, response, database }) {
   const { status } = request.query;
 
+  const filters = status ? { status } : null;
+
   // console.log(status); => closed, open...
 
-  const tickets = database.select("tickets");
+  const tickets = database.select("tickets", filters);
 
   return response.end(JSON.stringify(tickets));
 }
