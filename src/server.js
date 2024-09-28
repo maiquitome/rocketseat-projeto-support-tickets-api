8bcce962c48019dd16ac3aa3fbb538033460ce0b
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-27 21:54:03 -0300

    feat: :sparkles: add jsonHandler middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import http from "node:http"; // indica que o http faz parte do node, que não é uma dependência externa
 
-function listener(request, response) {
-  // Função.
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+
+async function listener(request, response) {
+  await jsonHandler(request, response);
+  console.log(request.body);
 }
 
 // const server = http.createServer((request, response) => {
-//   Função.
+//   jsonHandler...
 // })
 
 // const server = http.createServer(listener);
 
 // server.listen(3333);
 
+// * o código comentado acima é resumido pelo código abaixo:
 http.createServer(listener).listen(3333);
