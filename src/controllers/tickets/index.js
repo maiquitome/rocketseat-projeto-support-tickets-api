c98b9f0724cd8d1f205d717abc6de70ab1a8e9ed
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-28 18:23:03 -0300

    feat: :sparkles: add query params

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,4 +1,8 @@
 export function index({ request, response, database }) {
+  const { status } = request.query;
+
+  // console.log(status); => closed, open...
+
   const tickets = database.select("tickets");
 
   return response.end(JSON.stringify(tickets));
--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,14 +1,26 @@
 import { Database } from "../database/database.js";
 import { routes } from "../routes/index.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url;
+    // console.log("route:", route);
+    // console.log("test:", route.path.test(request.url));
+
+    // return route.method === request.method && route.path === request.url;
+    return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
+    const routeParams = request.url.match(route.path);
+    // console.log("routeParams:", routeParams);
+
+    const { query } = routeParams.groups;
+    // console.log("extractQueryParams", extractQueryParams(query));
+    request.query = query ? extractQueryParams(query) : {};
+
     return route.controller({ request, response, database });
   }
 
